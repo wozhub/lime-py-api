f03c8089b4018844e04b98dbdadffb82afa334d8
David Vinazza <dvinazza@estambul>
2014-07-08 17:55:22 -0300

    correciones de formato

--- limesurvey.py
+++ limesurvey.py
@@ -5,23 +5,23 @@ import json
 import sys
 from csv import DictReader
 
+
 class Api:
     def __init__(self, url, user, pw):
         self.url = url
         self._user = user
         self._password = pw
 
-        data="""{   "id": 1,
+        data = """{   "id": 1,
                     "method": "get_session_key",
                     "params": { "username": "%s",
                                 "password": "%s" } } """ % (user, pw)
 
         self.session_key = self._obtenerJson(data)['result']
 
-
-    def _obtenerJson(self,data):
-        #print data
-        req = urllib2.Request(url=self.url,data=data)
+    def _obtenerJson(self, data):
+        # print data
+        req = urllib2.Request(url=self.url, data=data)
         req.add_header('content-type', 'application/json')
         req.add_header('connection', 'Keep-Alive')
 
@@ -32,19 +32,19 @@ class Api:
 
         except:
             e = sys.exc_info()[0]
-            print ( "<p>Error: %s</p>" % e )
-
+            print ("<p>Error: %s</p>" % e)
 
     def list_surveys(self):
         json_list_surveys = self._list_surveys()
 
         encuestas=[]
         for e in json_list_surveys:
-            encuesta=e['sid'],e['surveyls_title'] # Me quedo con el SID y el Titulo
+            encuesta = e['sid'], e['surveyls_title']
+            # Me quedo con el SID y el Titulo
+
             encuestas.append(encuesta)
 
         return encuestas
-
 
     def _list_surveys(self):
         """Devuelve el JSON ENTERO"""
@@ -54,16 +54,14 @@ class Api:
 
         return self._obtenerJson(data)['result']
 
-
-    def activate_survey(self,sid):
+    def activate_survey(self, sid):
         data = """{ "id": 1,
                     "method": "activate_survey",
                     "params": { "sSessionKey": "%s",
-                                "SurveyID": %d } }""" % (self.session_key,sid)
+                                "SurveyID": %d } }""" % (self.session_key, sid)
         return self._obtenerJson(data)['result']
 
-
-    def import_survey(self,datos,titulo,sid):
+    def import_survey(self, datos, titulo, sid):
         data = """{ "id": 1,
                     "method": "import_survey",
                     "params": { "sSessionKey": "%s",
@@ -74,37 +72,32 @@ class Api:
                                 % (self.session_key, datos, titulo, sid)
         return self._obtenerJson(data)['result']
 
-
     def release_session_key(self):
         data = """ { "method": "release_session_key",
                      "params": { "sSessionKey" : "%s"},
                      "id":1}' }""" % (self.session_key)
         return self._obtenerJson(data)['result']
 
-
-    def export_responses(self,sid):
-        data=""" {          "method":"export_responses",
+    def export_responses(self, sid):
+        data = """ {          "method":"export_responses",
                             "params": { "sSessionKey": "%s",
                                         "iSurveyID":  %d,
                                         "DocumentType": "csv",
                                         "ResponseType": "long",
                                         "sHeadingType": "full" },
-                            "id": 1 } """ % (self.session_key,sid)
+                            "id": 1 } """ % (self.session_key, sid)
         return self._obtenerJson(data)['result']
 
-
-    def _add_response(self,sid,datos):
-        data=""" {          "method":"add_response",
-                            "params": { "sSessionKey": "%s",
-                                        "iSurveyID": %d,
-                                        "aResponseData": %s },
-                            "id": 1 } """ % (self.session_key,sid,datos)
+    def _add_response(self, sid, datos):
+        data = """ {          "method":"add_response",
+                              "params": { "sSessionKey": "%s",
+                                          "iSurveyID": %d,
+                                          "aResponseData": %s },
+                            "id": 1 } """ % (self.session_key, sid, datos)
         return self._obtenerJson(data)['result']
 
-
-    def importar_desde_archivo(self,sid,archivo):
+    def importar_desde_archivo(self, sid, archivo):
         respuestas = DictReader(open(archivo))
 
         for r in respuestas:
-            self._add_response(sid,json.dumps(r))
-
+            self._add_response(sid, json.dumps(r))
